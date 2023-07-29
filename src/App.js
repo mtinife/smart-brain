f2ebfc3876f9fe999b7d7fd65c5e637344b229a7
Micah Tinife <micahking2k@gmail.com>
2023-07-29 00:01:42 -0400

    added sign in feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Navigation from './components/Navigation/Navigation.js'
+import Signin from './components/Signin/Signin.js'
+import Register from './components/Register/Register.js'
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
@@ -8,6 +11,41 @@ import './App.css';
 const app = new Clarifai.App({
   apiKey: '603e20294a4c4472aa24f42c8946e81d'
  });
+
+const setupClarifaiRequestOptions = (imageUrl) => {
+  // Clarifai Personal Access Token
+  const PAT = 'bedbe6718d9c4a7a8d6848b3d6ddc8a5'
+  const USER_ID = 'mtinife'
+  const APP_ID = 'smart-brain'
+  const MODEL_ID = 'face-detection'
+  const IMAGE_URL = 'imageUrl'
+  const raw = JSON.stringify({
+    "user_app_id": {
+      "user_id": USER_ID,
+      "app_id": APP_ID
+    },
+    "inputs": [
+      {
+        "data": {
+          "image": {
+            "url": IMAGE_URL
+          }
+        }
+      }
+    ]
+  })
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Accept': 'Application/json',
+      'Authorization': 'Key ' + PAT
+    },
+    body: raw
+  }
+
+  return requestOptions
+}
 
 class App extends Component {
   constructor() {
@@ -42,21 +80,40 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models.predict(
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", setupClarifaiRequestOptions(this.state.input))
+    .then(response => response.json())
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
       .catch(err => console.log(err));
   }
 
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route})
+  }
+
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state
     return (
       <div className="App">
-        <Logo />
-        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home'
+          ? <div>
+              <Logo />
+              <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />    
+              <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+            </div>
+          : (
+            route === 'signin'
+            ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+          )
+        }
       </div>
-    );
+   );
   }
 }
 
